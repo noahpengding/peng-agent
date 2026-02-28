486246133f6314b62c4b64224b98ba4daac64f8d
Yipeng Ding <dingyipeng@dingyipeng.com>
2026-02-28 13:29:18 -0500

    Tavily Fix
    Related to #142

--- server/services/tools/search_tools.py
+++ server/services/tools/search_tools.py
@@ -11,6 +11,20 @@ def _wikipedia_search(query) -> str:
     results = search.run(query)
     return results
 
+def requests_toolkit():
+    from langchain_community.agent_toolkits.openapi.toolkit import RequestsToolkit
+    from langchain_community.utilities.requests import TextRequestsWrapper
+
+    HEADERS = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
+    toolkit = RequestsToolkit(
+        requests_wrapper=TextRequestsWrapper(headers=HEADERS),
+        allow_dangerous_requests=True,
+    )
+    tools = toolkit.get_tools()
+    return tools
+
 
 def _tavily_search(query: str, topic="general") -> str:
     from tavily import TavilyClient
@@ -33,6 +47,21 @@ def _tavily_search(query: str, topic="general") -> str:
         for result in response["images"]
     ]
 
+def _tavily_extract(url: str) -> str:
+    from tavily import TavilyClient
+
+    client = TavilyClient(
+        api_key=config.tavily_api_key,
+    )
+    response = client.extract(
+        urls=[url],
+        extract_depth="advanced",
+    )
+    return [
+        f"{result['title']} --- {result['raw_content']}"
+        for result in response["results"]
+    ]
+
 def _tavily_crawler(url: str, instructions: str) -> str:
     from tavily import TavilyClient
 
@@ -48,7 +77,7 @@ def _tavily_crawler(url: str, instructions: str) -> str:
         allow_external=False,
     )
     return [
-        f"{result['title']} --- {result['url']}: {result['content']}"
+        f"{result['url']}: {result['raw_content']}"
         for result in response["results"]
     ]
 
@@ -72,7 +101,7 @@ tavily_search_tool = StructuredTool.from_function(
 tavily_crawler_tool = StructuredTool.from_function(
     func=_tavily_crawler,
     name="tavily_crawler_tool",
-    description="A web crawler using Tavily (SaaS remote provider) that crawls a given url and extract relevant information based on the given instructions. Input should be a url and instructions for crawling. Use this tool when you have a specific url to start with",
+    description="A web crawler using Tavily (SaaS remote provider) that crawls a given url and extract relevant information based on the given instructions. Input should be a url and instructions for crawling. Use this tool when you have a specific url to start with and want to crawl the url and extract relevant information based on the instructions. The instructions should be specific about what information to extract",
     args_schema={
         "type": "object",
         "properties": {
@@ -90,7 +119,24 @@ tavily_crawler_tool = StructuredTool.from_function(
     return_direct=False,
 )
 
-tavily_tools = [tavily_search_tool, tavily_crawler_tool]
+tavily_extract_tool = StructuredTool.from_function(
+    func=_tavily_extract,
+    name="tavily_extract_tool",
+    description="A web content extractor using Tavily (SaaS remote provider) that extract raw content from a given url. Input should be a url. Use this tool when you have a specific url to start with and want to extract the raw content from the url without crawling other websites",
+    args_schema={
+        "type": "object",
+        "properties": {
+            "url": {
+                "type": "string",
+                "description": "The url to extract content from.",
+            },
+        },
+        "required": ["url"],
+    },
+    return_direct=False,
+)
+
+tavily_tools = [tavily_search_tool, tavily_crawler_tool, tavily_extract_tool]
 
 wikipedia_search_tool = StructuredTool.from_function(
     func=_wikipedia_search,
@@ -108,3 +154,5 @@ wikipedia_search_tool = StructuredTool.from_function(
     },
     return_direct=False,
 )
+
+requests_tools = requests_toolkit()
